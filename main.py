db7a460a781fcc67a04f5f637a65fabaf7f9e444
Griffin Bentley <griffinbentley00@gmail.com>
2022-03-20 02:08:45 -0700

    added memory mechanism

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+input_tiles = [2, 1, 1, 0, 2, 1, 2]
+
+# memory mechanism
+tiles = []
 for i in range(7):
+    tiles.append(input_tiles[i])
     for j in range(i+1):
         if j != i:
-            print(j, end=',')
+            print(tiles[j], end=',')
         else:
-            print(j)
+            print(tiles[j])
