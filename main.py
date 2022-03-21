0ea2313b530039d84a5aad8c1f47686f9932df1d
Griffin Bentley <griffinbentley00@gmail.com>
2022-03-21 02:14:40 -0700

    working memory mechanism that tells player what to press based off of previous keypresses

--- main.py
+++ main.py
@@ -1,12 +1,58 @@
-from screenshot import Screenshot
+#from screenshot import Screenshot
+import keyboard
+import time
+import random
 
-# memory mechanism
-tiles = []
-for i in range(7):
-    # get tile from screenshot
-    tiles.append(Screenshot())
-    for j in range(i+1):
-        if j != i:
-            print(tiles[j], end=',')
-        else:
-            print(tiles[j])
+key = 0
+
+def callback(event):
+    global key
+    name = event.name
+    if name == '1' or name == '2' or name == '3':
+        key = int(name)
+    elif name == 'f':
+        key = 4
+    elif name == 'esc':
+        key = 5
+    else:
+        pass
+
+def main():
+    global key
+    while key != 4:
+        keyboard.on_press(callback)
+        time.sleep(.05)
+    time.sleep(1)
+    if key == 5:
+        quit()
+    key = 0
+
+    # memory mechanism
+    tiles = []
+    for i in range(6):
+        # get tile from screenshot
+        #tiles.append(Screenshot())
+        print("go")
+        while key == 0:
+            keyboard.on_press(callback)
+            time.sleep(.05)
+        time.sleep(3 + i * .9)
+        if key == 5 or key == 4:
+            quit()
+        tiles.append(key)
+        key = 0
+        for j in range(i + 1):
+            k = tiles[j]
+            if j > 0 and tiles[j-1] == k:
+                time.sleep(.6 + random.random() * .05)
+            elif j > 1 and tiles[j-2] == k:
+                time.sleep(.5 + random.random() * .05)
+            elif j > 2 and tiles[j-3] == k:
+                time.sleep(.4 + random.random() * .05)
+            else:
+                time.sleep(.2 * random.random() * .05)   
+            print("press: ", k)
+    print("go")
+
+while True:
+    main()
--- screenshot.py
+++ screenshot.py
@@ -4,9 +4,18 @@ import pyautogui
 
 def Screenshot():
     # takes screenshot and saves on drive
-    image = pyautogui.screenshot()
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    cv2.imwrite("image.png", image)
+    # image = pyautogui.screenshot()
+    # image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    # cv2.imwrite("image.png", image)
+    image = cv2.imread("test/gw003.jpg")
+    lower = [np.array([22, 93, 0], dtype="uint8")]
+    upper = [np.array([120, 255, 255], dtype="uint8")]
+
+    # yellow
+    # np.array([22, 93, 0], dtype="uint8")
+    # np.array([45, 255, 255], dtype="uint8")
+    mask = cv2.inRange(image, lower[0], upper[0])
+    cv2.imwrite("image_y.jpg", mask)
 
     tile = 0
     return tile
