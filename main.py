2660ac2d09e835346ffb4260d46edbf518f61ba9
Griffin Bentley <griffinbentley00@gmail.com>
2022-03-24 03:19:22 -0700

    mostly working version using screenshots instead of user input to collect and print commands

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#from screenshot import Screenshot
 import keyboard
 import time
 import random
+import numpy as np
+from screenshot import Screenshot
+
+import cv2
 
 key = 0
 
@@ -22,37 +25,56 @@ def main():
     while key != 4:
         keyboard.on_press(callback)
         time.sleep(.05)
-    time.sleep(1)
+    #print("screenshot")
+    base_ryb = Screenshot()
     if key == 5:
         quit()
     key = 0
+    time.sleep(.5)
 
     # memory mechanism
     tiles = []
-    for i in range(6):
-        # get tile from screenshot
-        #tiles.append(Screenshot())
-        print("go")
-        while key == 0:
-            keyboard.on_press(callback)
-            time.sleep(.05)
-        time.sleep(3 + i * .9)
-        if key == 5 or key == 4:
-            quit()
-        tiles.append(key)
-        key = 0
+    for i in range(7):
+        #print("screenshot")
+        ryb= Screenshot()
+        sub = ryb - base_ryb
+        #print("ryb: ", ryb, "\nbase_ryb: ", base_ryb, "\nsub: ", sub)
+        if sub[0] > sub[1] and sub[0] > sub[2]:
+            #print(2)
+            tiles.append("2")
+        elif sub[1] > sub[0] and sub[1] > sub[2]:
+            #print(3)
+            tiles.append("3")
+        elif sub[2] > sub[0] and sub[2] > sub[1]:
+            #print(1)
+            tiles.append("1")
+        else:
+            tiles.append("c")
+        time.sleep(.75)
+
+        # while key == 0:
+        #     keyboard.on_press(callback)
+        #     time.sleep(.05)
+        # time.sleep(3 + i * .9)
+        # if key == 5 or key == 4:
+        #     quit()
+        # tiles.append(key)
+        # key = 0
         for j in range(i + 1):
             k = tiles[j]
             if j > 0 and tiles[j-1] == k:
                 time.sleep(.6 + random.random() * .05)
             elif j > 1 and tiles[j-2] == k:
-                time.sleep(.5 + random.random() * .05)
+                time.sleep(.55 + random.random() * .05)
             elif j > 2 and tiles[j-3] == k:
-                time.sleep(.4 + random.random() * .05)
+                time.sleep(.5 + random.random() * .05)
             else:
                 time.sleep(.2 * random.random() * .05)   
             print("press: ", k)
-    print("go")
+        time.sleep(2.25 + i * .75)
 
 while True:
     main()
+
+
+#cv2.imwrite("image_y.jpg", Screenshot(cv2.imread("test/gw001.jpg")))
--- screenshot.py
+++ screenshot.py
@@ -2,20 +2,44 @@ import numpy as np
 import cv2
 import pyautogui
 
+def Color_Sum(lower, upper, image):
+    lower, upper = np.array(lower), np.array(upper)
+    mask = cv2.inRange(image, lower, upper)
+    return int(np.sum(mask)/255), mask
+
+def Get_Colors(image):
+    sum_r, mask= Color_Sum([0, 50, 160], [9, 255, 255], image)
+    # if mode == "b":
+    #     cv2.imwrite("base_mask_r.jpg", mask)
+    # else:
+    #     cv2.imwrite("mask_r.jpg", mask)
+    sum_y, mask= Color_Sum([25, 50, 70], [35, 255, 255], image)
+    # if mode == "b":
+    #     cv2.imwrite("base_mask_y.jpg", mask)
+    # else:
+    #     cv2.imwrite("mask_y.jpg", mask)
+    sum_b, mask= Color_Sum([90, 50, 70], [128, 255, 255], image)
+    # if mode == "b":
+    #     cv2.imwrite("base_mask_b.jpg", mask)
+    # else:
+    #     cv2.imwrite("mask_b.jpg", mask)
+    return np.array([sum_r, sum_y, sum_b])
+
 def Screenshot():
-    # takes screenshot and saves on drive
-    # image = pyautogui.screenshot()
-    # image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    # cv2.imwrite("image.png", image)
-    image = cv2.imread("test/gw003.jpg")
-    lower = [np.array([22, 93, 0], dtype="uint8")]
-    upper = [np.array([120, 255, 255], dtype="uint8")]
+    image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2HSV)
+    image = image[:int(image.shape[0]/2),:,:]
+    # if mode == "b":
+    #     cv2.imwrite("base.jpg", cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
+    # else:
+    #     cv2.imwrite("image_y.jpg", cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
+    #image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2HSV)
+    #print(Get_Colors(image))
+    #image[image.shape[0]-1, image.shape[1]-1] = [255, 255, 255]
+    #image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    #return image
+    # if mode == "b":
+    #     return Get_Colors(image, mode)
+    # else:
+    #     return Get_Colors(image)
+    return Get_Colors(image)
 
-    # yellow
-    # np.array([22, 93, 0], dtype="uint8")
-    # np.array([45, 255, 255], dtype="uint8")
-    mask = cv2.inRange(image, lower[0], upper[0])
-    cv2.imwrite("image_y.jpg", mask)
-
-    tile = 0
-    return tile
