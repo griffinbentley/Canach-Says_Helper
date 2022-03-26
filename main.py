236eb32c85f38a5b7a47e951d746d2c446daf5b5
Griffin Bentley <griffinbentley00@gmail.com>
2022-03-25 23:56:50 -0700

    cleaned up code base and added comments

--- main.py
+++ main.py
@@ -4,62 +4,42 @@ import random
 import numpy as np
 from screenshot import Screenshot
 
-import cv2
-
 key = 0
 
+# tracks pressed keys
 def callback(event):
     global key
     name = event.name
-    if name == '1' or name == '2' or name == '3':
-        key = int(name)
-    elif name == 'f':
-        key = 4
-    elif name == 'esc':
-        key = 5
-    else:
-        pass
+    if name == 'f':
+        key = 1
 
-def main():
-    global key
-    while key != 4:
-        keyboard.on_press(callback)
-        time.sleep(.05)
-    #print("screenshot")
-    base_ryb = Screenshot()
-    if key == 5:
-        quit()
-    key = 0
-    time.sleep(.5)
-
+def memory(base_ryb, charges=3, debug=False):
     # memory mechanism
     tiles = []
+    # for each round of "Canach Says"
     for i in range(7):
-        #print("screenshot")
-        ryb= Screenshot()
+        if debug: print("screenshot")
+        ryb = Screenshot(debug)
+        # assign symbol based on difference in color between the two images
         sub = ryb - base_ryb
-        #print("ryb: ", ryb, "\nbase_ryb: ", base_ryb, "\nsub: ", sub)
+        if debug: print("ryb: ", ryb, "\nbase_ryb: ", base_ryb, "\nsub: ", sub)
         if sub[0] > sub[1] and sub[0] > sub[2]:
-            #print(2)
+            if debug: print(2)
             tiles.append("2")
         elif sub[1] > sub[0] and sub[1] > sub[2]:
-            #print(3)
+            if debug: print(3)
             tiles.append("3")
         elif sub[2] > sub[0] and sub[2] > sub[1]:
-            #print(1)
+            if debug: print(1)
             tiles.append("1")
+        # if no difference can be found, print error message and terminate
         else:
-            tiles.append("c")
+            print("Could not detect difference in source and new image\nTerminating program")
+            return 1
         time.sleep(.75)
 
-        # while key == 0:
-        #     keyboard.on_press(callback)
-        #     time.sleep(.05)
-        # time.sleep(3 + i * .9)
-        # if key == 5 or key == 4:
-        #     quit()
-        # tiles.append(key)
-        # key = 0
+        # tell player which keys to press with precise timings so that they don't try to press a key
+        #  while that key is still on cooldown
         for j in range(i + 1):
             k = tiles[j]
             if j > 0 and tiles[j-1] == k:
@@ -70,11 +50,33 @@ def main():
                 time.sleep(.5 + random.random() * .05)
             else:
                 time.sleep(.2 * random.random() * .05)   
-            print("press: ", k)
-        time.sleep(2.25 + i * .75)
+            print("press: ", k)       
+        if i != 6:
+            time.sleep(2.25 + i * .75)
+        else:
+            time.sleep(1)
+    # assumes player starts with 4 jade bot charges, and counts down games until player must collect more
+    if charges == 0:
+        print("collect 4 charges")
+        main()
+    print("press f")
+    memory(base_ryb, charges - 1)
+    return 0
 
-while True:
-    main()
+def main(debug=False):
+    global key
+    # wait for the user to press 'f' and start the first game
+    while key != 1:
+        keyboard.on_press(callback)
+        time.sleep(.05)
+    if debug: print("screenshot")
+    # take the base screenshot for the others to be compared to
+    base_ryb = Screenshot(debug)
+    key = 0
+    time.sleep(.5)
 
+    # if memory() returns an error, end program
+    if memory(base_ryb, debug): 
+        return
 
-#cv2.imwrite("image_y.jpg", Screenshot(cv2.imread("test/gw001.jpg")))
+main()
--- screenshot.py
+++ screenshot.py
@@ -2,44 +2,25 @@ import numpy as np
 import cv2
 import pyautogui
 
-def Color_Sum(lower, upper, image):
+# sums up number of pixel within given HSV range
+def Color_Sum(lower, upper, image, debug=False):
     lower, upper = np.array(lower), np.array(upper)
     mask = cv2.inRange(image, lower, upper)
+    if debug: print(mask)
     return int(np.sum(mask)/255), mask
 
-def Get_Colors(image):
-    sum_r, mask= Color_Sum([0, 50, 160], [9, 255, 255], image)
-    # if mode == "b":
-    #     cv2.imwrite("base_mask_r.jpg", mask)
-    # else:
-    #     cv2.imwrite("mask_r.jpg", mask)
-    sum_y, mask= Color_Sum([25, 50, 70], [35, 255, 255], image)
-    # if mode == "b":
-    #     cv2.imwrite("base_mask_y.jpg", mask)
-    # else:
-    #     cv2.imwrite("mask_y.jpg", mask)
-    sum_b, mask= Color_Sum([90, 50, 70], [128, 255, 255], image)
-    # if mode == "b":
-    #     cv2.imwrite("base_mask_b.jpg", mask)
-    # else:
-    #     cv2.imwrite("mask_b.jpg", mask)
+# gathers sums of the three colors of given image
+def Get_Colors(image, debug=False):
+    sum_r, mask= Color_Sum([0, 50, 160], [9, 255, 255], image, debug)
+    if debug: cv2.imwrite("mask_r.jpg", mask)
+    sum_y, mask= Color_Sum([25, 50, 70], [35, 255, 255], image, debug)
+    if debug: cv2.imwrite("mask_y.jpg", mask)
+    sum_b, mask= Color_Sum([90, 50, 70], [128, 255, 255], image, debug)
+    if debug: cv2.imwrite("mask_b.jpg", mask)
     return np.array([sum_r, sum_y, sum_b])
 
-def Screenshot():
+# takes screenshot and returns color sums for red, yellow and blue
+def Screenshot(debug=False):
     image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2HSV)
     image = image[:int(image.shape[0]/2),:,:]
-    # if mode == "b":
-    #     cv2.imwrite("base.jpg", cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
-    # else:
-    #     cv2.imwrite("image_y.jpg", cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
-    #image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2HSV)
-    #print(Get_Colors(image))
-    #image[image.shape[0]-1, image.shape[1]-1] = [255, 255, 255]
-    #image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
-    #return image
-    # if mode == "b":
-    #     return Get_Colors(image, mode)
-    # else:
-    #     return Get_Colors(image)
-    return Get_Colors(image)
-
+    return Get_Colors(image, debug)
